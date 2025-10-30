9bd88535a96416d8c1cfb444d42cc555a147f36e
seanpatel15 <spate758@illinois.edu>
2025-10-30 22:09:25 +0000

    Updated backend and packages json to include script for testing with Jest + added tests file in backend

---
+++ backend/__tests__/test.js
@@ -0,0 +1,8 @@
+const { describe, it, expect } = require('@jest/globals');
+
+describe('Example test', () => {
+  it('adds numbers correctly', () => {
+    const sum = 2 + 3;
+    expect(sum).toBe(5);
+  });
+});
